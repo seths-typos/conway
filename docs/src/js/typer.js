69cd484c8fae7806e51f660dc40cb047f84a5281
seths-typos <juicegoode@gmail.com>
2024-12-23 16:35:20 -0800

    Add numbers and punctuation to typer

--- docs/src/js/typer.js
+++ docs/src/js/typer.js
@@ -1,9 +1,22 @@
 function typeString (str) {
+  GAME.reset()
   for (const LTR in str) {
     let char = str[LTR];
 
     if (char == " ") {
       GAME.addSpace();      
+    } else if (/[\n]/.test(char)) {
+      GAME.carriageReturn();
+    } else if (/[\-]/.test(char)){
+        GAME.typeLetter(LETTERS[CUR_FONT]["-"], "m");
+    } else if (/[\']/.test(char)){
+        GAME.typeLetter(LETTERS[CUR_FONT]["Quote"], "t");    
+    } else if (/[\"]/.test(char)){
+        GAME.typeLetter(LETTERS[CUR_FONT]["DoubleQuote"], "t");    
+    } else if (/[*^]/.test(char)){
+        GAME.typeLetter(LETTERS[CUR_FONT][char], "t");    
+    } else if (/[\d]/.test(char)){
+        GAME.typeLetter(LETTERS[CUR_FONT]["Digit" + char]);    
     } else {
       GAME.typeLetter(LETTERS[CUR_FONT][char]);
     }
